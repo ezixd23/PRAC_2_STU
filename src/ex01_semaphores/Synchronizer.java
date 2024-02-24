56f389b088a5753144633c9731117f946f40dd7c
zelkarrabi <ziad.elkarrabi@gmail.com>
2024-02-24 18:37:33 +0100

    start ex1

--- src/ex01_semaphores/Synchronizer.java
+++ src/ex01_semaphores/Synchronizer.java
@@ -8,6 +8,11 @@ public class Synchronizer {
 	 * Semaphores and only semaphores. Nothing else, not even simple-typed variables
 	 */
 	
+	private Semaphore pitStop = new Semaphore(1);
+	private Semaphore rentry = new Semaphore(0);
+	private Semaphore raising = new Semaphore(0);
+	private Semaphore lowering = new Semaphore(0);
+	private Semaphore replacement = new Semaphore(0);
 	
 	private volatile int currentCar = -1; // the id of the car being serviced
 	private Kerberos analyser = new Kerberos(); // the trace analyser
@@ -16,20 +21,28 @@ public class Synchronizer {
 	
 	public void startRaising () {
 		/* COMPLETE */
+		raising.acquireUninterruptibly();
 	}
 	
 	public void endRaising () {
 		analyser.writeString("Car["+currentCar+"] is UP"); // leave this line at the very beginning
 		/* COMPLETE */
+		replacement.release();
+		replacement.release();
+		replacement.release();
+		replacement.release();
 	}
 	
 	public void startLowering () {
 		/* COMPLETE */
+		lowering.acquireUninterruptibly();
 	}
 	
 	public void endLowering () {
 		analyser.writeString("Car["+currentCar+"] is DOWN"); // leave this line at the very beginning
 		/* COMPLETE */
+		rentry.release();
+		
 	}
 	
 	
@@ -37,11 +50,13 @@ public class Synchronizer {
 	
 	public void startReplacement() {
 		/* COMPLETE */
+		replacement.acquireUninterruptibly();
 	}
 	
 	public void endReplacement(String changer) {
 		analyser.writeStringSync("\tTYRE REPLACED["+currentCar+"]: "+changer); // leave this line at the very beginning
 		/* COMPLETE */
+		lowering.release();
 	}
 	
 	// --- operations invoked by CAR
@@ -49,15 +64,18 @@ public class Synchronizer {
 	
 	public void pitStop(int car) {
 		/* COMPLETE */
+		pitStop.acquireUninterruptibly();
 		
 		analyser.writeString("REPLACEMENT starts for car["+car+"]");
 		currentCar = car;
 		
 		/* COMPLETE */
-		
+		raising.release();
+		rentry.acquireUninterruptibly();
 		analyser.writeString("Car["+car+"] RE-ENTERS circuit\n");
 
-		/* COMPLETE */ 
+		/* COMPLETE */
+		pitStop.release();
 	}
 	
 }
