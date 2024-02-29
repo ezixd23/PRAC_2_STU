058d280c92904794d97049fae7cbfbef5fbae15e
Roger Esteve <rogeresteve30@gmail.com>
2024-02-29 16:43:45 +0100

    ex2: working on heavy traffic

--- src/ex02_monitor/BridgeMonitor.java
+++ src/ex02_monitor/BridgeMonitor.java
@@ -16,6 +16,13 @@ public class BridgeMonitor {
 	/* Declare lock, condition objects and all the simple-typed variables that
 	 * you deem necessary to solve the problem
 	 */
+	private final Lock lock = new ReentrantLock();
+	Condition northQueue = lock.newCondition();
+	Condition southQueue = lock.newCondition();
+
+	private int northCount = 0;
+	private int southCount = 0;
+	private int inTransit = 0;
 	
 	// Constructor. Do not modify it
 	public BridgeMonitor (int maxWhenWaiting, StateViewer viewer) {
@@ -26,44 +33,82 @@ public class BridgeMonitor {
 	
 	public void letMeGoSouth (int id) {
 		/* COMPLETE */
+		this.lock.lock();
+		if(southBarrier == CLOSED)
+			southQueue.awaitUninterruptibly();
 		
 		safetyAnalyzer.readyToGoSouth(id); // do not remove
 		
 		
 		/* COMPLETE */
+		this.southCount++;
 		
 		safetyAnalyzer.goingSouth(id); // do not remove
 		
 		/* COMPLETE */
+		this.inTransit++;
+		this.lock.unlock();
 	}
 	
 	public void southReached (int id) {
 		/* COMPLETE */
+		this.lock.lock();
 		
 		safetyAnalyzer.southReached(id); // do not remove
 		
 		/* COMPLETE */
+		this.inTransit--;
+
+		if (this.southCount >= MAX) {
+			this.southCount = 0;
+			this.southBarrier = CLOSED;
+			this.northBarrier = OPEN;
+			this.northQueue.signal();
+		} else {
+			if(this.inTransit == 0) this.southQueue.signal();
+		}
+		this.lock.unlock();
 	}
 	
 	//-----
 	
 	public void letMeGoNorth (int id) {
+		this.lock.lock();
 		/* COMPLETE */
+		if(northBarrier == CLOSED)
+			northQueue.awaitUninterruptibly();
 		
 		safetyAnalyzer.readyToGoNorth(id); // do not remove
 		
 		/* COMPLETE */
+		this.northCount++;
 		
 		safetyAnalyzer.goingNorth(id); // do not remove
 		/* COMPLETE */
+		this.inTransit++;
+		this.lock.unlock();
 	}
 	
 	public void northReached (int id) {
 		/* COMPLETE */
+		this.lock.lock();
 		
 		safetyAnalyzer.northReached(id); // do not remove
 		
 		/* COMPLETE */
+		System.out.println(this.northCount);
+		System.out.println(this.inTransit);
+		this.inTransit--;
+		if (this.northCount >= MAX) {
+			this.northCount = 0;
+			this.northBarrier = CLOSED;
+			this.southBarrier = OPEN;
+			this.southQueue.signal();
+		} else {
+			if(this.inTransit == 0) this.northQueue.signal();
+		}
+
+		this.lock.unlock();
 	}
 	
 }
